af0ec83a87cdbf0fe650ad89c486b6a7ced8578a
PopovychVasyl <hraq300011@gmail.com>
2024-05-01 18:48:08 +0300

    Update

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,25 +1,47 @@
+let index = 0;
+let img = [document.getElementById('img1'),document.getElementById('img2'),document.getElementById('img3'),document.getElementById('img4')];
+let dots = [document.getElementById('dot1'),document.getElementById('dot2'),document.getElementById('dot3'),document.getElementById('dot4')];
 
-const slides = document.querySelectorAll('.slide');
-const dots = document.querySelectorAll('.dot');
-let currentSlide = 0;
+let timerImage = setInterval(()=>currentSlide(index + 1), 4000);
 
-function updateDots() {
-  dots.forEach(dot => dot.classList.remove('active'));
-  dots[currentSlide].classList.add('active');
+let currentSlide = function(n){
+  console.log("n = "+n);
+  console.log("inx = "+index);
+  clearInterval(timerImage);
+  if(n === -1){
+    n = 3;
+  }
+  if(n === 4){
+    n = 0;
+  }
+    if(n != index){ 
+      dots[index].style.backgroundColor = '#bbb';
+      dots[n].style.backgroundColor = 'rgb(223, 143, 58)';
+
+      for (let i = 0; i < img.length; i++) {
+        if(i!=n && i!=index){
+          img[i].style.zIndex = 1;
+        }
+        img[i].style.opacity = 1;
+        img[i].classList.remove('fade-out');
+        img[i].classList.remove('fade-in');
+      }
+
+      img[index].style.zIndex = 10;
+      img[n].style.zIndex = 9;
+
+      img[index].classList.add('fade-out');
+      img[n].classList.add('fade-in');
+
+      index = n;
+    }
+    timerImage = setInterval(()=>currentSlide(index + 1), 4000);
 }
 
-function showSlide(slideIndex) {
-  slides.forEach(slide => slide.classList.remove('active'));
-  slides[slideIndex].classList.add('active');
-}
+let nextSlide = function(){
+  currentSlide(index + 1);
+} 
 
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
-  updateDots();
-}
-
-slides[currentSlide].classList.add('active');
-updateDots();
-
-setInterval(nextSlide, 3000);
+let prevSlide = function(){
+  currentSlide(index - 1);
+} 
